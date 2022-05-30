ccf2b051bd2baeac000880311ca087a839374d4a
cristianbl <crblanaru@gmail.com>
2022-05-30 11:43:00 +0200

    actual files

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print ('Hello!')
+from fastapi import FastAPI
+import os
+
+app = FastAPI()
+
+
+@app.get("/")
+def hello_world():
+    api_key = os.getenv('API_KEY')
+    return {"bunnyshell-neo": "awesome", "api_key": api_key}
